66968c2866c15074e8991158214c7e9ddfd3ea4d
NooraAbdullah <noorasubaiee@gmail.com>
2020-01-13 14:04:32 +0300

    Nav completed

--- src/App.js
+++ src/App.js
@@ -4,14 +4,31 @@ import {Route, Link} from 'react-router-dom'
 import Ticket from './components/ticket/Ticket'
 import category from './components/data/category'
 import Nav from './components/nav'
+
+import Nav from './components/nav/Nav'
+import Category from './components/category/Category'
+
+
 function App() {
   return (
     <div className="App">
-      <Route path="/" component={Nav}/>
-      <Ticket Ticket={category.data} />
-      
+
+    <div className="App">
+    <div> <Route path="/" component={Nav}/> </div>
+      <div className= "category">
+        <Route path="/" component={Category}/> 
+        {/* <Route exact path="/banks" component={Banks}/> 
+        <Route path="/" component={}/> 
+        <Route path="/" component={}/> 
+        <Route path="/" component={}/> 
+        <Route path="/" component={}/>  */}
+      </div>    
+    <Ticket Ticket={category.data} />
     </div>
-  );
+      
+
+
+  )
 }
 
 export default App;
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
 
-function Nav () {
+function Nav() {
     return (
-      <h1> My Ques </h1>
-      <h1> Nearby Ques </h1>
-      <h1> Find Ques </h1>
-      <h1> My Ques </h1>
-    )
+        <div className="nav-div">
+            <Link to="/My-qs"> <nav className="Nav navbar navbar-light bg-light"> 
+            <span className="navbar-brand mb-0 h1">My Ques </span>
+            </nav></Link>
+            <Link to="/Nearby-q"><nav className="navbar navbar-light bg-light">
+            <span className="navbar-brand mb-0 h1">Nearby Ques</span>
+            </nav></Link>
+            <Link to="/find-q"><nav className="navbar navbar-light bg-light">
+            <span className="navbar-brand mb-0 h1">Find Ques</span>
+            </nav></Link>
+            <Link to="/my-ticket"><nav className="navbar navbar-light bg-light">
+            <span className="navbar-brand mb-0 h1">My Ticket</span>
+            </nav></Link>
+      </div>
+    ) 
 }
 export default Nav
