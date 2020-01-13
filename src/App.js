0a3696d2108e93165812a7c071c579787599bea7
NooraAbdullah <noorasubaiee@gmail.com>
2020-01-13 14:04:32 +0300

    Nav completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import './App.css';
 import {Route, Link} from 'react-router-dom' 
-import Nav from './components/nav'
+import Nav from './components/nav/Nav'
+import Category from './components/category/Category'
 function App() {
   return (
     <div className="App">
-      <Route path="/" component={Nav}/>
-      
+     <div> <Route path="/" component={Nav}/> </div>
+      <div className= "category">
+        <Route path="/" component={Category}/> 
+        {/* <Route exact path="/banks" component={Banks}/> 
+        <Route path="/" component={}/> 
+        <Route path="/" component={}/> 
+        <Route path="/" component={}/> 
+        <Route path="/" component={}/>  */}
+      </div>      
     </div>
-  );
+  ); //link to
 }
 
 export default App;
