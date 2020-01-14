657ede8869a39ebf1b98a1d5a0cfccb7d161a6dc
roush55 <ranaabumelha@gmail.com>
2020-01-14 17:02:36 +0300

    show categories id

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import './App.css';
 import {Route, Link} from 'react-router-dom' 
 import Ticket from './components/ticket/Ticket'
-import category from './components/data/category'
+//import category from './components/data/category'
 import data from './components/data/data'
 import Nav from './components/nav/Nav'
 import Categories from './components/category/Categories'
+import Category from   './components/category/Category'
 //import images from './images'
 
 
@@ -24,8 +25,16 @@ return (
         {/* <Ticket Ticket={category.data} /> */}
       
       {/* <Category/> */}
+      <Route exact path='/categories/:id' render={(props) => {
+        const id = props.match.params.id
+      // let ourdata = data.filter(data => data.id == id)
+      return (
+      <Category
+        data={data[id]}
+        />
+        )} }/>
 
-      <Route  path='/' render={(props) => (
+      <Route exact path='/' render={(props) => (
       <div>
         {data.map(data => (
           <Categories
--- src/components/data/data.js
+++ src/components/data/data.js
@@ -7,77 +7,98 @@ const data=[
     name:"Cheescake factory",
     image:"image_url",
     crowded: false,
-    count: 0},
+    count: 0,
+    services:"tabel"
+},
 
     {id: 1,
     catagory:"Resturant",
     name:"P.F. Chang's",
     image:"https://static.wixstatic.com/media/bd92e4_4a4601d7c0bf4adeb607ea5f62487100~mv2.png/v1/fill/w_1024,h_768,al_c,q_90/file.jpg",
     crowded: false,
-    count: 0},
+    count: 0,
+    services:"Table"},
 
     {id: 2,
     catagory:"Resturant",
     name:"Cioccolati Italiani",
     image:"https://i2.wp.com/www.foodmakers.it/wp-content/uploads/2018/11/CIOCCOLATI-ITALIANI-DOHA-QATAR-3.jpg?resize=800%2C536&ssl=1",
     crowded: false,
-    count: 0},
+    count: 0,
+    services:"Table"
+},
 
     {id: 3,
     catagory:"Coffe",
     name:"JOLT",
     image:"image_url",
     crowded: false,
-    count: 0},
+    count: 0,
+    services:"Table"
+},
 
     {id: 4,
     catagory:"Coffe",
     name:"Dose",
     image:"image_url",
     crowded: false,
-    count: 0},
+    count: 0,
+    services:"Table"
+},
 
     {id: 5,
     catagory:"Bank",
     name:"Samba",
     image:"image_url",
     crowded: false,
-    count: 0},
+    count: 0,
+    services:"Customer Service"
+},
 
     {id: 6,
     catagory:"Bank",
     name:"NCB",
     image:"image_url",
     crowded: false,
-    count: 0},
+    count: 0,
+    services:"Customer Service"
+},
 
     {id: 7,
     catagory:"Bank",
     name:"Riyadh Bank",
     image:"image_url",
     crowded: false,
-    count: 0},
+    count: 0,
+    services:"Customer Service"
+},
 
     {id: 8,
     catagory:"Hospital",
     name:"Dallah",
     image:"image_url",
     crowded: false,
-    count: 0},
+    count: 0,
+    services:"Customer Service"
+},
 
     {id: 9,
     catagory:"Government",
     name:"Ministry of Health",
     image:"image_url",
     crowded: false,
-    count: 0},
+    count: 0,
+    services:"Customer Service"
+},
 
     {id: 10,
     catagory:"Government",
     name:"Passports",
     image:"image_url",
     crowded: false,
-    count: 0}
+    count: 0,
+    services:"Customer Service"
+}
 
 
 ]
