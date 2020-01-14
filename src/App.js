3a3f65fcfb0af594ab212629cb9c09ae06f851ee
roush55 <ranaabumelha@gmail.com>
2020-01-14 14:24:26 +0300

    create catogries in the root page & props

--- src/App.js
+++ src/App.js
@@ -16,7 +16,8 @@ import Ticket from './components/ticket/Ticket'
 import category from './components/data/category'
 import data from './components/data/data'
 import Nav from './components/nav/Nav'
-import Category from './components/category/Category'
+import Categories from './components/category/Categories'
+//import images from './images'
 
 
 function App() {
@@ -24,7 +25,7 @@ return (
     <div className="App">
       {/* <div> <Route path="/" component={Nav}/> </div>
         <div className= "category">
-          <Route path="/" component={Category}/> 
+          {/* <Route path="/" component={Category}/>  */}
           {/* <Route exact path="/banks" component={Banks}/> 
           <Route path="/" component={}/> 
           <Route path="/" component={}/> 
@@ -37,8 +38,24 @@ return (
 
         {/* <Ticket Ticket={category.data} /> */}
       
-      <Category/>
-      
+      {/* <Category/> */}
+
+      <Route  path='/' render={(props) => (
+      <div>
+        {data.map(data => (
+          <Categories
+              key={data.id}
+              name={data.name}
+              image={data.image}
+               id={data.id}
+              crowded={data.crowded}
+              catagory={data.Category}
+
+          />
+        ))}
+      </div>
+    )}
+    />
     </div>
 
       
