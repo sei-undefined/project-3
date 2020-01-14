720f59564aff006f265be2903199da2436168455
roush55 <ranaabumelha@gmail.com>
2020-01-14 17:07:50 +0300

    ticket component

--- src/components/ticket/Ticket.js
+++ src/components/ticket/Ticket.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
-// import category from '../data/category'
+//import category from './data/category'
+import Moment from 'react-moment';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Ticket extends Component {
     state = {  }
     render() { 
-       let  allCatogry=this.props.Ticket;
-        return (<div>
+        const dateToFormat = '1976-04-19T12:59-0500';
+            
+ 
         
-    <p><h1>Your Ticket:</h1>{allCatogry.map(element=> <li>{element.name}</li>)}</p>
-        </div>
+       let  allCatogry=this.props.Ticket;
+        return (<div className="container">
+            
+           
+        <div id="accordion" role="tablist">
+        <div className="card">
+            <div className="card-header"><h2>E-Ticket:</h2>
+            <Moment locale="de">{dateToFormat}</Moment></div>
+      <div className="card-body">
+          <p className="card-title">{this.props.object.name}</p>
+        <button className="btn btn-primary" onClick={() => this.props.delete(this.props.object.id)}>Delete{this.props.id}</button></div></div>
+        </div></div>
         );
     }
 }
