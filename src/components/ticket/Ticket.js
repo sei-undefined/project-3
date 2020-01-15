428c342a79d6a04f20f25184445030a3d1fb0283
roush55 <ranaabumelha@gmail.com>
2020-01-15 15:43:38 +0300

    style ticket && services

--- src/components/ticket/Ticket.js
+++ src/components/ticket/Ticket.js
@@ -5,7 +5,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import category from '../data/category'
 import {connect} from 'react-redux';
 import {withRouter} from 'react-router-dom';
-
+import images from "../../images/images";
+ //import Ticket from 'ticket.css'
 class Ticket extends Component {
     state = {  }
     render() { 
@@ -27,18 +28,25 @@ class Ticket extends Component {
         <div className="container">   
             <div id="accordion" role="tablist">
                 <div className="card">
-                    <div className="card-header">
+                    <div className="card-header headert">
                         <h2>E-Ticket:</h2>
-                        <h2>{`You are ${data.tickets}`}</h2>
+                        
                         <Moment locale="de">{dateToFormat}</Moment>
                     </div>
                     <div className="card-body">
                         <p className="card-title">{data.name}</p>
+                        <h2>{`You are ${data.tickets}`}</h2>
+                        
+                        
                         {/* <button className="btn btn-primary" onClick={() => this.props.delete(this.props.object.id)}>Delete{this.props.id}</button></div></div> */}
                     </div>
+                   
+                     
+                              </div>
+                              <img src={images[11]} alt="hour" width="200px"/>
                 </div>
             </div>
-        </div>
+    
         );
         // componentDidMount(){
 
