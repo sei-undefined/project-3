94b73ef3301c6160a93a42b7678b8345e4433051
ahmedalhindi <alhindi.ahmed@gmail.com>
2020-01-15 10:43:46 +0300

    connected Ticket componenet and fixed route

--- src/components/ticket/Ticket.js
+++ src/components/ticket/Ticket.js
@@ -3,28 +3,55 @@ import React, { Component } from 'react';
 import Moment from 'react-moment';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import category from '../data/category'
+import {connect} from 'react-redux';
+import {withRouter} from 'react-router-dom';
 
 class Ticket extends Component {
     state = {  }
     render() { 
-        
+        // finding id
+        const id=this.props.match.params.id
+        let data = this.props.queuesData
+        data = data.find(queue=>queue.id ==id)
+
         const dateToFormat = '1976-04-19T12:59-0500';
             
  
         
-       let  allCatogry=this.props.Ticket;
+    //    let  allCatogry=this.props.Ticket;
         return (
         <div className="container">   
-        <div id="accordion" role="tablist">
-        <div className="card">
-        <div className="card-header"><h2>E-Ticket:</h2>
-        <Moment locale="de">{dateToFormat}</Moment></div>
-        <div className="card-body">
-        <p className="card-title">{this.props.object.name}</p>
-        <button className="btn btn-primary" onClick={() => this.props.delete(this.props.object.id)}>Delete{this.props.id}</button></div></div>
-        </div></div>
+            <div id="accordion" role="tablist">
+                <div className="card">
+                    <div className="card-header">
+                        <h2>E-Ticket:</h2>
+                        <Moment locale="de">{dateToFormat}</Moment>
+                    </div>
+                    <div className="card-body">
+                        <p className="card-title">{data.name}</p>
+                        {/* <button className="btn btn-primary" onClick={() => this.props.delete(this.props.object.id)}>Delete{this.props.id}</button></div></div> */}
+                    </div>
+                </div>
+            </div>
+        </div>
         );
     }
 }
- 
+
+const mapStateToProps = (state) =>{
+    return{
+        queuesData: state.queuesData
+    }
+}
+const mapDispatchToProps = (dispatch) =>{
+    return{
+      getTicket: (num)=> dispatch({
+        type: "GET_TICKET",
+        value: num})
+    }
+}
+
+
+Ticket = withRouter(Ticket)
+Ticket = connect(mapStateToProps,mapDispatchToProps)(Ticket) 
 export default Ticket;
